ba8930619d54e9fd0d6daf5f112b339243188367
Melissa <mpangilinan@ischool.berkeley.edu>
2014-10-28 01:50:14 -0700

    finished q1

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,6 +1,6 @@
 class Trainer < ActiveRecord::Base
 
-	has many :pokemons
+	has_many :pokemons
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
